c3d1c0065a6de449f903d9a9cf6d0f8b3cb0387d
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2022-11-14 23:20:08 -0800

    add documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Round [![Go Reference](https://pkg.go.dev/badge/github.com/portfoliotree/round.svg)](https://pkg.go.dev/github.com/portfoliotree/round)
+
+Helps you round floating point numbers in large structures.
+
+This is useful for to properly render numbers.
---
+++ example_test.go
@@ -0,0 +1,114 @@
+package round_test
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/portfoliotree/round"
+)
+
+type Data struct {
+	Number   float64
+	Map      map[FloatKey]float64 `precision:"3"`
+	Embedded struct {
+		SomeRate float64 `precision:"2,percent"`
+	}
+	List []float64
+}
+
+func Example() {
+	var data Data
+	data.Number = 1.1111
+	data.Map = map[FloatKey]float64{
+		4.5555555: 4.5555555,
+		4.4444444: 4.4444444,
+	}
+	data.Embedded.SomeRate = 0.998765
+	data.List = []float64{7, 6.656}
+
+	fmt.Printf("before: %s\n", data.String())
+	_ = round.FloatsRecursively(&data, 2)
+	fmt.Printf("after:  %s", data.String())
+
+	// Output: before: {
+	// 	"Number": 1.1111,
+	// 	"Map": {
+	// 		"4.4444444": 4.4444444,
+	// 		"4.5555555": 4.5555555
+	// 	},
+	// 	"Embedded": {
+	// 		"SomeRate": 0.998765
+	// 	},
+	// 	"List": [
+	// 		7,
+	// 		6.656
+	// 	]
+	// }
+	// after:  {
+	// 	"Number": 1.11,
+	// 	"Map": {
+	// 		"4.4444444": 4.444,
+	// 		"4.5555555": 4.556
+	// 	},
+	// 	"Embedded": {
+	// 		"SomeRate": 99.88
+	// 	},
+	// 	"List": [
+	// 		7,
+	// 		6.66
+	// 	]
+	//}
+}
+
+func (d Data) String() string {
+	buf, err := json.MarshalIndent(d, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	return string(buf)
+}
+
+type FloatKey float64
+
+func (fk FloatKey) MarshalText() (text []byte, err error) {
+	return []byte(fmt.Sprintf("%g", float64(fk))), nil
+}
+
+/*
+--- FAIL: Example (0.00s)
+got:
+
+want:
+before: {
+        "Number": 1.1111,
+        "Map": {
+                "4.4444444": 4.4444444,
+                "4.5555555": 4.5555555
+        },
+        "Embedded": {
+                "SomeRate": 0.998765
+        },
+        "List": [
+                7,
+                6.656
+        ]
+}
+after:  {
+        "Number": 1.11,
+        "Map": {
+                "4.4444444": 4.444,
+                "4.5555555": 4.556
+        },
+        "Embedded": {
+                "SomeRate": 99.88
+        },
+        "List": [
+                7,
+                6.66
+        ]
+}
+FAIL
+exit status 1
+FAIL	github.com/portfoliotree/round	0.104s
+
+*/
